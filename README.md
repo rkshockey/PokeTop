7d16b927a03db5ea3a682a92d3f6c1cc43eb76b3
rkshockey <rkshockey@gmail.com>
2021-08-16 06:59:16 -0400

    Added redux and deleted obsolete files

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # PokeTop
-A program mean for a hybrid tabletop RPG/ video game for Pokemon.
+A program meant for a hybrid tabletop RPG/ video game for Pokemon.
 
 ============
 CURRENT BUGS
 ============
 HordeVariants page:
     -The page will freeze or crash if any input box has more than 3 digits.
-    -Variant counts will continue to climb whenever the number is changed until the 'Types of Pokemon' field is cleared.
+    -Variant counts will continue to climb while number is being entered, hit 'submit' for an accurate read
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import HordesVariants from './components/HordesVariants';
-import PokeCards from './components/PokeCards';
 import './App.css';
 import { Link, Route, Switch } from 'react-router-dom'
 
@@ -10,15 +9,11 @@ function App() {
         <nav>
           <Link to='/'><button>Home</button></Link>
           <Link to='/hordes-variants'><button>Hordes</button></Link>
-          <Link to='/pokecards'><button>PokeCards</button></Link>
         </nav>
       </header>
       <Switch>
         <Route path='/hordes-variants'>
           <HordesVariants />
-        </Route>
-        <Route path='/pokecards'>
-          <PokeCards />
         </Route>
       </Switch>
     </div>
